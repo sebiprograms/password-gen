7c9620a8d88f78ed9eaf05f7434b33ddfc9a6eff
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2024-06-10 21:05:49 -0500

    Add random variable and assign string to pass with for loop

--- password_gen.cpp
+++ password_gen.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <string>
 
-std::string alphabet = {'a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+std::string alphabet_ = {'a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
                        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
                                                          'w', 'x', 'y', 'z'};
 
 int main() 
 {
   // Creating Logic
-  std::cout << "Press Y/N to generate a password\n";
+  std::cout << "One password ready hot'n fresh!\n";
+
   std::string pass[8];
 
   //assinging data
-  for(int i = 0; i <= 8; i++)
+  for(int i = 0; i < 8; i++)
   {
-    pass[i];
+    int random = rand() % 26;
+    pass[i] = alphabet[random];
+
   }
 
 
+
   //outputting data
+  for(int i = 0; i < 8; i++)
+  {
+    std::cout << pass[i]; 
+  }
+
+
 
   return 0;
 }
