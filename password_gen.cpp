6bcaf3ece041df58040b694b7fd82ef4007719cf
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2024-06-10 20:39:48 -0500

    Created password_gen.cpp

---
+++ password_gen.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+std::string alphabet = {'A'};
+
+int main() 
+{
+  // Creating Logic
+  std::cout << "Press Y/N to generate a password\n";
+  std::string pass[8];
+
+  //assinging data
+  for(int i = 0; i < 10; i++)
+  {
+    pass[i] = rand
+  }
+
+
+  //outputting data
+  
+  return 0;
+}
